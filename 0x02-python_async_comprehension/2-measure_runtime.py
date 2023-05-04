95725c5977f0111712071d3c1f92f5d94b60be7d
Ken-Obieze <kenneth.obieze@outlook.com>
2023-05-04 18:28:28 +0100

    fix

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,6 +10,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measure runtime of coroutine.""" 
     start_time = time.time()
-    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    await asyncio.gather(*(async_comprehension() for _ in  rangen(4)))
     end_time = time.time()
     return (end_time - start_time)
