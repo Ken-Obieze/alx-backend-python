63a9a04d5b30cc56726dcc1649de039ad952a6b7
Ken-Obieze <kenneth.obieze@outlook.com>
2023-05-04 14:13:13 +0100

    task 0fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator():
     """generate async element."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random(0, 10)
+        yield random.uniform(0, 10)
