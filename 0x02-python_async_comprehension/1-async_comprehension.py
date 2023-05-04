f28a36966a6b91eeeb72f998f646f8e665b80904
Ken-Obieze <kenneth.obieze@outlook.com>
2023-05-04 17:54:29 +0100

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""async_generator coroutine module."""
+
+import random
+import asyncio
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    numbers = [number async for number in async_generator()]
+    return numbers
