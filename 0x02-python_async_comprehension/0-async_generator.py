ee720fa48cb38c0b34c0f4fc3f23ca73372207bf
Ken-Obieze <kenneth.obieze@outlook.com>
2023-05-04 18:32:39 +0100

    fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """Generate async element."""
     for _ in range(10):
         await asyncio.sleep(1)
