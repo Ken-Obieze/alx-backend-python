40f24fc1f904bb9ac32ddca882035613a787f7fc
Ken-Obieze <kenneth.obieze@outlook.com>
2023-05-12 03:08:07 +0100

    fix

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Module to return asyncio.Task."""
 import asyncio
-from typing import Any
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Waits for random implementation."""
-    return asyncio.create_task(wait_random(max_delay))
+    task: asyncio.Task = asyncio.create_task(wait_random(max_delay))
+    return task
