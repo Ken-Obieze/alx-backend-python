45953dd2eb9142631ba804c0e6c6bdeda3f1a60e
ken-obieze <kenneth.obieze@outlook.com>
2025-09-28 15:55:13 +0100

    messaging app auth fix2

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -131,6 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
